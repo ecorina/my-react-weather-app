7e58195a4de90c899042dbf59fba78f1268d4949
ecorina <serban.corrina@gmail.com>
2022-03-08 19:01:19 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
+import { isNonNullChain } from "typescript";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Corina S and is {""}
-        <a
-          href="https://github.com/ecorina/my-react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <input className="form-control" type="text" placeholder="City..." />
+        Weather App
+        <footer>
+          This project was coded by Corina S and is {""}
+          <a
+            href="https://github.com/ecorina/my-react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
