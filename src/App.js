87e78e5fe61a64fd5d870e5781a9651d4470b3a1
ecorina <serban.corrina@gmail.com>
2022-03-08 18:43:23 +0000

    add rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ export default function App() {
       Weather App
       <footer>
         This project was coded by Corina S and is {""}
-        <a href="https://github.com/ecorina/my-react-weather" target="_blank">
+        <a
+          href="https://github.com/ecorina/my-react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
           open-sourced on GitHub
         </a>
