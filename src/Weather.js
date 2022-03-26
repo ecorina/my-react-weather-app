1da2be9fab90ec25d7c2c56d4183943aae0ecee8
ecorina <serban.corrina@gmail.com>
2022-03-26 10:59:34 +0000

    added api key and url

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,10 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
 export default function Weather() {
+  const apiKey = `6905ff0b38fa30ab82c89c7372272fbd`;
+  let city = "London";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   return (
     <div className="weather">
       <form>
